5cb9e0b4555d948c70d25965f225cd1ef78be0a2
Sreejit <sreesen2003@gmail.com>
2023-05-25 20:49:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 function get() {
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'X-RapidAPI-Key': process.env.API_KEY,
-    //         'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-    //     }
-    // };
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': process.env.API_KEY,
+            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+        }
+    };
     
-    // fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-    //     .then(response => response.json())
-    //     .then(response => document.querySelector(".quote").innerHTML = `"${response.content}" <p class="author">~${response.originator.name}</p>`)
-    //     .catch(err => console.error(err));
-
-    const url = 'https://nimble-longma-24b282.netlify.app/.netlify/functions/fetchAPI'
-    const response = fetch(url)
-    const data = response.json();
-    console.log(data);
+    fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+        .then(response => response.json())
+        .then(response => document.querySelector(".quote").innerHTML = `"${response.content}" <p class="author">~${response.originator.name}</p>`)
+        .catch(err => console.error(err));
 }
