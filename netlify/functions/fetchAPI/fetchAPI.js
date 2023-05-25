4202c8beb53dae6e10db569a9a22f5f57d7e72f5
Sreejit <sreesen2003@gmail.com>
2023-05-25 20:06:23 +0530

    Testing netlify cli

--- netlify/functions/fetchAPI/fetchAPI.js
+++ netlify/functions/fetchAPI/fetchAPI.js
@@ -1,31 +1,30 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-  // try {
-  //   const subject = event.queryStringParameters.name || 'World'
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({ message: `Hello ${subject}` }),
-  //     // // more keys you can return:
-  //     // headers: { "headerName": "headerValue", ... },
-  //     // isBase64Encoded: true,
-  //   }
-  // } catch (error) {
-  //   return { statusCode: 500, body: error.toString() }
-  // }
-
-     const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': process.env.API_KEY,
-            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-        .then(response => response.json())
-        // .then(response => document.querySelector(".quote").innerHTML = `"${response.content}" <p class="author">~${response.originator.name}</p>`)
-        .then(console.log(response))
-        .catch(err => console.error(err));
+  const apiKey = process.env.API_KEY
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+          'X-RapidAPI-Key': apiKey,
+          'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+      }
+  };
+  
+  fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+      // .then(response => response.json())
+      // .then(response => document.querySelector(".quote").innerHTML = `"${response.content}" <p class="author">~${response.originator.name}</p>`)
+      // .then(console.log(response))
+      // .catch(err => console.error(err));
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
 }
 
 module.exports = { handler }
