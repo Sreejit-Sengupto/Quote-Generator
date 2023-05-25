09d69635725b73f4602b65036b0227ac05c44426
Sreejit <sreesen2003@gmail.com>
2023-05-25 20:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function get() {
     //     .then(response => document.querySelector(".quote").innerHTML = `"${response.content}" <p class="author">~${response.originator.name}</p>`)
     //     .catch(err => console.error(err));
 
-    const url = 'https://nimble-longma-24b282.netlify.app/.netlify/functions/fetchAI'
+    const url = 'https://nimble-longma-24b282.netlify.app/.netlify/functions/fetchAPI'
     const response = fetch(url)
     const data = response.json();
     console.log(data);
